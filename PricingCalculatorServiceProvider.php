30ee2cb094beb5be3bdcdc16a7e204de8616c90c
Ivan Mijatovic <ivanmijatovic89@gmail.com>
2023-11-30 18:12:11 +0100

    first

---
+++ PricingCalculatorServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bookaweb\PricingCalculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class PricingCalculatorServiceProvider extends ServiceProvider
+{
+    // private $namespace = 'App\Ads\Controllers';
+
+    // public function boot()
+    // {
+
+    // }
+    // public function register()
+    // {
+
+    // }
+}
---
+++ src/PricingService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Bookaweb\PricingCalculator;
+
+class PricingService
+{
+    public static function calculate()
+    {
+        return 'mijat';
+    }
+}
